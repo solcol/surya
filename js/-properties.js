f7fd79c5d63ac6adc4ab03c81fe9dc623ab2544d
Tom <git@tomr.me>
2022-03-04 12:48:35 +0000

    jekyll-generated properties

--- js/-properties.js
+++ js/-properties.js
@@ -1,110 +1,25 @@
+---
+---
 // we can use the variable "properties" in our other JS files to get access to this list
 // on a production website, these would be pulled from a database rather than a file like this
 
-const properties = [{
-    "title": "Bates Lane, Tanworth",
-    "type": "house",
-    "is_for": "purchase",
-    "rights": "Freehold",
-    "price": 2350000,
-    "beds": 5,
-    "baths": 4,
-    "images": [
-        "img/houses/2/outside.jpeg",
-        "img/houses/2/garden.jpeg",
-        "img/houses/2/kitchen.jpeg",
-        "img/houses/2/garages.jpeg",
-        "img/houses/2/pool.jpeg"
-    ],
-    "url": "property2.html",
-    "weight": Math.random()
-},
-
-{
-    "title": "Gentlemans Lane, Henley",
-    "type": "house",
-    "is_for": "purchase",
-    "rights": "Freehold",
-    "price": 3350000,
-    "beds": 6,
-    "baths": '6',
-    "images": [
-        "img/houses/1/outside.jpeg",
-        "img/houses/1/garden.jpeg",
-        "img/houses/1/livingroom.jpeg",
-        "img/houses/1/pool.jpeg"
-    ],
-    "url": "property1.html",
-    "weight": Math.random()
-},
-
-{
-    "title": "Stratford Road, Wootton Wawen",
-    "type": "house",
-    "is_for": "purchase",
-    "rights": "Freehold",
-    "price": 450000,
-    "beds": 3,
-    "baths": 1,
-    "images": [
-        "img/houses/3/outside.jpeg",
-        "img/houses/3/garden.jpeg",
-        "img/houses/3/kitchen.jpeg",
-    ],
-    "url": "property3.html",
-    "weight": Math.random()
-},
-
-{
-    "title": "High Street, Henley",
-    "type": "house",
-    "is_for": "purchase",
-    "rights": "Freehold",
-    "price": 975000,
-    "beds": 4,
-    "baths": 2,
-    "images": [
-        "img/houses/5/outside.jpeg",
-        "img/houses/5/garden.jpeg",
-        "img/houses/5/kitchen.jpeg",
-    ],
-    "url": "property5.html",
-    "weight": Math.random()
-},
-
-{
-    "title": "Buckley Green, Henley",
-    "type": "house",
-    "is_for": "purchase",
-    "rights": "Freehold",
-    "price": 2450000,
-    "beds": 4,
-    "baths": '?',
-    "images": [
-        "img/houses/6/outside.jpeg",
-        "img/houses/6/garden.jpeg",
-        "img/houses/6/kitchen.jpeg",
-        "img/houses/6/pool.jpeg"
-    ],
-    "url": "property6.html",
-    "weight": Math.random()
-},
-
-{
-    "title": "Chadwick House, Station Road, Solihull",
-    "type": "flat",
-    "is_for": "purchase",
-    "rights": "Leasehold",
-    "price": 325000,
-    "beds": 2,
-    "baths": 2,
-    "images": [
-        "img/houses/4/outside.jpeg",
-        "img/houses/4/livingroom.jpeg",
-        "img/houses/4/bathroom.jpeg"
-    ],
-    "url": "property4.html",
-    "weight": Math.random()
-}
-
+const properties = [
+    {% for p in site.properties %}
+    {
+        "title": "{{ p.title }}",
+        "type": "{{ p.type }}",
+        "is_for": "{{ p.is_For }}",
+        "rights": "{{ p.rights }}",
+        "price": {{ p.price }},
+        "beds": {{ p.beds }},
+        "baths": {{ p.baths }},
+        "images": [
+            {% for i in p.images %}
+            "{{ i }}"{% if forloop.last != true %},{% endif %}
+            {% endfor %}
+        ],
+        "url": "property2.html",
+        "weight": Math.random()
+    }{% if forloop.last != true %},{% endif %}
+    {% endfor %}
 ];
